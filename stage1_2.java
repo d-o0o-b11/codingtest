c3ebe7b796d5ac18b134f12dc411a8fe40243f29
d-o0o-b11 <97296843+d-o0o-b11@users.noreply.github.com>
2022-01-20 17:23:59 +0900

    stage1
    baekjoon
    <Solving them step by step.>

---
+++ stage1_2.java
@@ -0,0 +1,11 @@
+package hihi;
+
+public class stage1_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+
+}
