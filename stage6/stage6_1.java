89d4074ebb1539e61930516b159b92300707f9b6
d-o0o-b11 <97296843+d-o0o-b11@users.noreply.github.com>
2022-01-26 13:21:10 +0900

    stage6
    baekjoon
    <Solving them step by step.>

---
+++ stage6/stage6_1.java
@@ -0,0 +1,25 @@
+package stage6;
+
+public class stage6_1 {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+}
+
+class Test{
+	long sum(int[]a) {
+		long sum1=0;
+		
+		for(int i=0;i<a.length;i++) {
+			sum1+=a[i];
+		}
+		
+		return sum1;
+	}
+}
